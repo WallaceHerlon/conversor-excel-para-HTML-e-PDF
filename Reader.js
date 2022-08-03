a26b1a5223317cd5f3bb40dcf5023536f401cb99
Wallace Herlon <wherlon@hotmail.com>
2022-08-02 22:35:48 -0300

    Reader

---
+++ Reader.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+class Reader {
+    Reader(filepath) {
+        fs.readFile(filepath, 'utf-8', (err, data) => {
+            if(err) {
+                console.log('err')
+            } else {
+                console.log(data)
+            }
+        })
+    }
+}
+
+module.exports = Reader
